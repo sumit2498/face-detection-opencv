9d6047b8a12dd3e781e4aa88c4476492310c1c94
Sumit Dantale <dantalesumit@gmail.com>
2020-02-05 12:42:44 +0530

    Labeling Remaining

---
+++ face.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x, y, w, h) in faces:
+      print(x,y,w,h)
+      roi_gray = gray[y:y+h, x:x+w]
+      roi_color = frame[y:y+h, x:x+w]
+      img_item = "faces.png"
+      cv2.imwrite(img_item,roi_gray)
+      color = (255,0,0)
+      stroke = 2
+      end_chord_x = x+w
+      end_chord_y = y+h
+      cv2.rectangle(frame,(x,y),(end_chord_x,end_chord_y),color,stroke)
+
+    # Our operations on the frame come here
+    
+   
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    # cv2.imshow('gray',gray)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
